fc41f79317c3cb41a04e8e455c89b47857d2ab62
sunkz <sunkezheng@outlook.com>
2019-11-19 18:55:54 +0800

    sidecar

--- sidecar/node-service-sidecar/src/main/java/com/sunkz/cloud/sidecar/node/service/SideCarApplication.java
+++ sidecar/node-service-sidecar/src/main/java/com/sunkz/cloud/sidecar/node/service/SideCarApplication.java
@@ -1,13 +1,13 @@
-package com.sunkz.cloud.nacos.provider;
+package com.sunkz.cloud.sidecar.node.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServiceProviderApplication {
+public class SideCarApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServiceProviderApplication.class, args);
+        SpringApplication.run(SideCarApplication.class, args);
     }
 
 }
