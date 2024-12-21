c9f65a61a8ad8bb0d657e1078489a51571177572
sunkz <sun_kz@icloud.com>
2024-12-21 23:10:18 +0800

    2024-12-21 23:10:18

--- nacos-discovery/nacos-consumer/src/main/java/com/sunkz/cloud/nacos/consumer/controller/EchoController.java
+++ nacos-discovery/nacos-consumer/src/main/java/com/sunkz/cloud/nacos/consumer/controller/EchoController.java
@@ -1,6 +1,7 @@
 package com.sunkz.cloud.nacos.consumer.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,11 +11,17 @@ import org.springframework.web.client.RestTemplate;
 @AllArgsConstructor
 public class EchoController {
 
-    private final RestTemplate restTemplate;
+    private final EchoClient echoClient;
 
     @GetMapping("/echo/{str}")
     public String echo(@PathVariable String str) {
-        return restTemplate.getForObject("http://nacos-service-provider/echo/" + str, String.class);
+        return echoClient.echo(str);
+    }
+
+    @FeignClient(name = "nacos-service-provider")
+    interface EchoClient {
+        @GetMapping("/echo/{string}")
+        String echo(@PathVariable String string);
     }
 
 }
